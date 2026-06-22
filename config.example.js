c41addf39627cbf5b2ca95d0f483d9a7c016a063
“Danilogit config --global user.email “daniloderango@gmail.com <tua@email.com>
2026-06-23 01:50:37 +0200

    update: final Google Apps Script deployment URL

--- config.example.js
+++ config.example.js
@@ -4,7 +4,7 @@
 
 window.ADMIN_CONFIG = {
   // URL del Google Apps Script endpoint
-  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwI0NiLJN4Flf4W9gfZaanVVvH1TUxH4LOswC8Dg2iYgN-kzP6TN6xkEDyZhJWYDGyuuA/exec',
+  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyroc5mPF6ml10Atd0wrLuxVTj-JLan9Qjh2xu7MrQTeO24I1s7djnL2ezuwMmM_V8urA/exec',
 
   // Token di autenticazione - Genera con setupAuthToken() nello Apps Script
   // IMPORTANTE: Esegui setupAuthToken() nello Apps Script editor per generare il token
